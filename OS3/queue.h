1280dc3e979244d071089c8c05a3e1e1751a008e
고광현 <go779918@gmail.com>
2025-06-13 14:19:47 +0900

    queue.h파일을 잘못 알아서 수정

--- OS3/queue.h
+++ OS3/queue.h
@@ -1,34 +1,35 @@
-#ifndef _QTYPE_H  // header guard
-#define _QTYPE_H
+#ifndef _QUEUE_H // header guard
+#define _QUEUE_H 
 
-// ==========이 파일은 수정 가능==========
+// ==========이 파일은 수정 불가==========
+// 참고: 일반적으로 queue_init()과 같이 prefix를 붙여 소속(?)을 명확히 함
+// 이 과제에서는 편의를 위해 짧은 이름을 사용
 
-typedef unsigned int Key;  // 값이 클수록 높은 우선순위
-typedef void* Value;
+#include "qtype.h"
 
-typedef struct {
-    Key key;
-    Value value;
-} Item;
+// 큐 초기화, 해제
+Queue* init(void);
+void release(Queue* queue);
 
-typedef struct {
-    bool success;   // true: 성공, false: 실패
-    Item item;
-    // 필드 추가 가능
-} Reply;
 
-typedef struct node_t {
-    Item item;
-    struct node_t* next;
-    // 필드 추가 가능
-} Node;
+// ==========concurrent operations==========
 
-typedef struct {
-    Node* head, tail;
-    // 필드 추가 가능
-} Queue;
+// 노드 생성&초기화, 해제, 복제
+Node* nalloc(Item item);
+void nfree(Node* node);
+Node* nclone(Node* node);
 
-// 이후 자유롭게 추가/수정: 새로운 자료형 정의 등
+// (key, item)을 키값에 따라 적절한 위치에 추가
+// Reply: 문제가 없으면 success=true, 아니면 success=false
+Reply enqueue(Queue* queue, Item item);
+
+//  첫 번째 노드(키값이 가장 큰 노드)를 추출(큐에서 삭제하고 아이템을 리턴)
+// Reply: 큐가 비어있으면 success=false
+//        아니면          success=true,  item=첫 번째 노드의 아이템
+Reply dequeue(Queue* queue);
+
+// start <= key && key <= end 인 노드들을 찾고,
+// 해당 노드들을 <복제>해서 새로운 큐에 삽입한 후 포인터를 리턴
+Queue* range(Queue* queue, Key start, Key end);
 
 #endif
-#pragma once
